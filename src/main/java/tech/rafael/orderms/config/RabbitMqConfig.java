b6ddfe1826603843213faeec84e59c91ef4813c6
Douglas Rafael <doodohrafael@gmail.com>
2026-02-16 17:52:20 -0300

    tb_orders mapeia a tabela de pedidos. public removido do metodo main. boas praticas, pois nao tem mais necessidade. Conversor de json JacksonJsonMessageConverter para mensagens de filas.

--- src/main/java/tech/rafael/orderms/config/RabbitMqConfig.java
+++ src/main/java/tech/rafael/orderms/config/RabbitMqConfig.java
@@ -1,10 +1,28 @@
 package tech.rafael.orderms.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
     public static final String ORDER_CREATED = "order-created";
+    public static final Logger logger = LoggerFactory.getLogger(RabbitMqConfig.class);
+
+    @Bean
+    public JacksonJsonMessageConverter jacksonJsonMessageConverter() {
+        logger.info("Json converted");
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public Declarable orderCreatedQueue() {
+        return new Queue(ORDER_CREATED);
+    }
 
 }
--- src/main/java/tech/rafael/orderms/entity/OrderEntity.java
+++ src/main/java/tech/rafael/orderms/entity/OrderEntity.java
@@ -1,4 +1,25 @@
 package tech.rafael.orderms.entity;
 
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Document(collection ="tb_orders")
 public class OrderEntity {
+
+    @MongoId
+    private Long orderId;
+
+    @Indexed(name = "customer_id_idx")
+    private Long customerId;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal total;
+
+    private List<OrderItem> items;
 }
