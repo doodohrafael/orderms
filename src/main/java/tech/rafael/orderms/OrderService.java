f3af163098290da66ba8cb918be6096c6d682ba5
Douglas Rafael <doodohrafael@gmail.com>
2026-03-03 12:12:03 -0300

    - servico que compoe regras de negocios relacionadas a pedidos.

---
+++ src/main/java/tech/rafael/orderms/OrderService.java
@@ -0,0 +1,41 @@
+package tech.rafael.orderms;
+
+import org.springframework.stereotype.Service;
+import tech.rafael.orderms.entity.OrderEntity;
+import tech.rafael.orderms.entity.OrderItem;
+import tech.rafael.orderms.listener.dto.OrderCreatedEvent;
+import tech.rafael.orderms.repository.OrderRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository repository;
+
+    public OrderService(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(OrderCreatedEvent event) {
+        var entity = new OrderEntity();
+        entity.setOrderId(event.orderId());
+        entity.setCustomerId(event.customerId());
+        entity.setItems(getOrderItems(event));
+        entity.setTotal(getTotal(event));
+        this.repository.save(entity);
+    }
+
+    private BigDecimal getTotal(OrderCreatedEvent event) {
+        return event.items().stream()
+                .map(item -> (BigDecimal.valueOf(item.quantity()).multiply(item.price())))
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    private static List<OrderItem> getOrderItems(OrderCreatedEvent event) {
+        return event.items().stream().map(item ->
+                new OrderItem(item.product(), item.quantity(), item.price())).toList();
+    }
+}
