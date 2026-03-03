0bc4142c59bf8c81780de9705b2727ece977f8ab
Douglas Rafael <doodohrafael@gmail.com>
2026-03-03 18:00:31 -0300

    - dto para suporte na resposta dos pedidos

---
+++ src/main/java/tech/rafael/orderms/controller/dto/OrderResponse.java
@@ -0,0 +1,14 @@
+package tech.rafael.orderms.controller.dto;
+
+import tech.rafael.orderms.entity.OrderEntity;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long orderId,
+                            Long customerId,
+                            BigDecimal total) {
+
+    public static OrderResponse fromEntity(OrderEntity entity) {
+        return new OrderResponse(entity.getOrderId(), entity.getCustomerId(), entity.getTotal());
+    }
+}
