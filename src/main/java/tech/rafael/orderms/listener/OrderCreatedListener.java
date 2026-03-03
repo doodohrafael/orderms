a33e260897acfb812a6cdefd758d98b127c86981
Douglas Rafael <doodohrafael@gmail.com>
2026-03-03 12:14:49 -0300

    - Injecao do servico de pedido no listener para delegar o conteudo das mensagens para o servico fazer a tratativa necessaria e logo apos a persistencia.

--- src/main/java/tech/rafael/orderms/listener/OrderCreatedListener.java
+++ src/main/java/tech/rafael/orderms/listener/OrderCreatedListener.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import tech.rafael.orderms.OrderService;
 import tech.rafael.orderms.listener.dto.OrderCreatedEvent;
 
 import static tech.rafael.orderms.config.RabbitMqConfig.ORDER_CREATED;
@@ -14,9 +15,16 @@ public class OrderCreatedListener {
 
     public static final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
 
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @RabbitListener(queues = ORDER_CREATED)
     public void listen(Message<OrderCreatedEvent> message) {
-        logger.info("Message consumed: {}, {}", message, "teste");
+        logger.info("Message consumed: {}", message);
+        this.orderService.save(message.getPayload());
     }
 
 }
